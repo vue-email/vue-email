be3776911d87a80a2479edeaac0f93400f312b78
Dave136 <davejs136@gmail.com>
2023-02-05 17:58:17 -0400

    add: utils

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+export { copyTextToClipboard } from './copy-text-to-clipboard';
+export { pxToPt } from './px-to-pt';
+export { withMargin, type Margin } from './with-margin';
+export { styleToString } from './style-to-string';
+export { unreachable } from './unreachable';
---
+++ src/utils/style-to-string.ts
@@ -0,0 +1,14 @@
+export const styleToString = (style: Record<string, string | number | null>) => {
+  return Object.keys(style).reduce(
+    (acc, key) =>
+      acc +
+      key
+        .split(/(?=[A-Z])/)
+        .join('-')
+        .toLowerCase() +
+      ':' +
+      style[key] +
+      ';',
+    ''
+  );
+};
