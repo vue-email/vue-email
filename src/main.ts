ee148e4acae398d66c91104fb048d84f4ec6c47e
Dave136 <davejs136@gmail.com>
2023-02-05 17:47:08 -0400

    feat: prepare lib

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue';
-import './style.css';
 import App from './app.vue';
 
 createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import eslingPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslingPlugin()],
+  build: {
+    target: 'esnext',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'vue-email',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 });
