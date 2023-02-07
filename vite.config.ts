2c996715599ba3adb71708eb5801c683cf78c180
Dave136 <davejs136@gmail.com>
2023-02-07 13:30:30 -0400

    feat: generate types

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
-import eslingPlugin from 'vite-plugin-eslint';
+import eslintPlugin from 'vite-plugin-eslint';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslingPlugin()],
+  plugins: [
+    vue(),
+    eslintPlugin(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     target: 'esnext',
     lib: {
