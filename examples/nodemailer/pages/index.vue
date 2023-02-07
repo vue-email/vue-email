a2018fdc42d5a2353671b5a90aabe67ff1270920
Dave136 <davejs136@gmail.com>
2023-02-07 14:25:03 -0400

    add: nodemailer example

---
+++ examples/nodemailer/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <h2>Send email with nodemailer</h2>
+  <button @click="handleClick">Sent email</button>
+  <div v-if="messageId.length && previewUrl.length">
+    <p>Email sent:</p>
+    <p>MessageId: {{ messageId }}</p>
+    <p>previewUrl: <a :href="previewUrl">Visit here</a></p>
+  </div>
+
+  <button @click="seeTemplate = !seeTemplate">See template component</button>
+
+  <div v-if="seeTemplate">
+    <welcome-template />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { render } from 'vue-email';
+import WelcomeTemplate from '../components/welcome-template.vue';
+const messageId = ref('');
+const previewUrl = ref('');
+const seeTemplate = ref(false);
+
+const handleClick = async () => {
+  const template = await render(WelcomeTemplate);
+  const data = await $fetch('/api/email', {
+    method: 'post',
+    body: {
+      template,
+    },
+  });
+  messageId.value = data.messageId;
+  previewUrl.value = data.previewUrl;
+};
+</script>
