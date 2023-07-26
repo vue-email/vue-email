65d82446fddd4c9e2f1a2ce95fc5b9e2d440a9d1
Younes Barrad <younesbarraddev@gmail.com>
2023-07-26 01:43:14 +0100

    fix: removed lock

--- demo/nuxt.config.ts
+++ demo/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxthq/ui'],
+  modules: ['@nuxthq/ui', 'vue-email/nuxt'],
   typescript: {
     shim: false,
   },
