05b2c25aed1eb250950661c9cceda86362a0e0af
Younes Barrad <younesbarraddev@gmail.com>
2023-07-26 01:28:00 +0100

    fix: disabled nuxt demo for now

--- demo/nuxt.config.ts
+++ demo/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxthq/ui', 'vue-email/nuxt'],
+  modules: ['@nuxthq/ui'],
   typescript: {
     shim: false,
   },
