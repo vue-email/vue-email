64528630fe1b94cb0be3ae23711eaa5f575ff4c5
David Arenas <davejs136@gmail.com>
2023-07-30 16:34:43 -0400

    feat: add descriptor file

---
+++ src/compiler/descriptor.ts
@@ -0,0 +1,24 @@
+import path from 'node:path'
+import { createHash } from 'node:crypto'
+import type * as _compiler from 'vue/compiler-sfc'
+
+interface ResolvedOptions {
+  compiler: typeof _compiler
+  root: string
+  isProd?: boolean
+}
+
+export function createDescriptor(filename: string, source: string, { root, isProd, compiler }: ResolvedOptions): _compiler.SFCParseResult {
+  const { descriptor, errors } = compiler.parse(source, {
+    filename,
+  })
+
+  const normalizedPath = path.normalize(path.relative(root, filename))
+  descriptor.id = generateId(normalizedPath + (isProd ? source : ''))
+
+  return { descriptor, errors }
+}
+
+function generateId(text: string) {
+  return createHash('sha256').update(text).digest('hex').substring(0, 8)
+}
