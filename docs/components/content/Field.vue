e8db8816eb5d799d105beecbf370c6bea596c3b3
Younes Barrad <younesbarraddev@gmail.com>
2023-07-31 19:24:00 +0100

    docs: fixed lint issue

--- docs/components/content/Field.vue
+++ docs/components/content/Field.vue
@@ -16,7 +16,7 @@ defineProps({
     type: Boolean,
     default: false,
   },
-  default: {
+  defaultVal: {
     type: String,
     default: '',
   },
@@ -29,7 +29,7 @@ defineProps({
       <div class="flex flex-1 gap-x-2.5">
         <span class="rounded-md font-semibold text-primary-500 dark:text-primary-400">{{ name }}</span>
         <span v-if="required" class="text-gray-500 dark:text-gray-400">required</span>
-        <span v-else-if="default" class="text-gray-500 dark:text-gray-400">Default: {{default}}</span>
+        <span v-else-if="defaultVal" class="text-gray-500 dark:text-gray-400"> Default: {{ defaultVal }} </span>
       </div>
       <div class="text-right text-gray-700 dark:text-gray-200">
         {{ type }}
@@ -37,9 +37,7 @@ defineProps({
     </div>
 
     <p v-if="description" class="mt-3 mb-0 text-gray-600 dark:text-gray-300 text-sm space-y-3">
-      <ContentSlot :use="$slots.default">
-        {{ description }}
-      </ContentSlot>
+      {{ description }}
     </p>
   </div>
 </template>
