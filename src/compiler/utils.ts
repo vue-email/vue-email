8d25ba08426c17bf640d8a31d1f6c7a9d7aeb3ff
David Arenas <davejs136@gmail.com>
2023-08-07 17:20:09 -0400

    refactor(compiler/utils): import just env from node:process

--- src/compiler/utils.ts
+++ src/compiler/utils.ts
@@ -1,7 +1,7 @@
-import * as process from 'node:process'
+import { env as $env } from 'node:process'
 
 export function env(name: string): string | null | undefined {
-  return process.env[name]
+  return $env[name]
 }
 
 export const isProd: boolean = env('NODE_ENV') === 'production'
