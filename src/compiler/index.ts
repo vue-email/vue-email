8446719089fa597314cd6a89a4564b22ece7ffad
David Arenas <davejs136@gmail.com>
2023-08-10 09:21:36 -0400

    refactor: remove no used compiler stuff in index file

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -1,17 +1,10 @@
 import { createInitConfig } from './config'
-import { compileTemplate, templateRender } from './template'
-import { getFilesRecursively } from './file'
+import { templateRender } from './template'
 
 import type { DefineConfig, Options, RenderOptions } from './types'
 
-export const defineConfig: DefineConfig = (config: Options) => {
+export const config: DefineConfig = (config: Options) => {
   const defaultConfig = createInitConfig(config)
-  const dir = config?.dir ?? defaultConfig?.dir
-  const files = getFilesRecursively(dir)
-
-  for (const path of files.filter((file) => !file.includes('.vuemail'))) {
-    compileTemplate(path, defaultConfig)
-  }
 
   return {
     render: (name: string, options?: RenderOptions): Promise<string> => templateRender(name, options, defaultConfig),
