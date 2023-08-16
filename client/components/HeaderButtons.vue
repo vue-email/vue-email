32ea58c48bbbae31d4c2fee5f613197d87dd59ef
Younes Barrad <younesbarraddev@gmail.com>
2023-08-17 00:44:11 +0100

    feat: added discord link

--- client/components/HeaderButtons.vue
+++ client/components/HeaderButtons.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const ui = { rounded: 'rounded-full' }
+</script>
 
 <template>
-  <div class="flex items-center justify-between gap-3">
+  <div class="flex items-center justify-between gap-2">
     <SearchButton />
     <UButton icon="i-ph-read-cv-logo-bold" size="sm" color="gray" variant="solid" label="Docs" to="https://www.vuemail.net/" target="_blank" />
-    <UButton icon="i-ph-github-logo-bold" size="sm" color="gray" variant="solid" to="https://github.com/Dave136/vue-email" target="_blank" />
+    <UButton icon="i-ph-github-logo-bold" :ui="ui" size="sm" color="gray" variant="solid" to="https://github.com/Dave136/vue-email" target="_blank" />
+    <UButton icon="i-ph-discord-logo-bold" :ui="ui" size="sm" color="blue" variant="solid" to="https://discord.gg/Z2RW7qvS3Q" target="_blank" />
   </div>
 </template>
