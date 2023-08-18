7aab330c3dfe919622d3ecb27780c631d7764817
David Arenas <davejs136@gmail.com>
2023-08-18 19:02:40 -0400

    feat: inject i18n on render composable

--- src/composables/render.ts
+++ src/composables/render.ts
@@ -6,7 +6,11 @@ import pretty from 'pretty'
 export interface Options {
   pretty?: boolean
   plainText?: boolean
-  i18n?: boolean
+  i18n?: {
+    locale: string
+    defaultLocale: string
+    translations: Record<string, Record<string, string>>
+  }
 }
 
 /**
@@ -22,10 +26,32 @@ export async function useRender(
   options: Options = {
     pretty: false,
     plainText: false,
+    i18n: undefined,
   },
 ) {
+  let vueI18n
+
+  try {
+    if (options?.i18n) {
+      vueI18n = await import('vue-i18n')
+    }
+  } catch (error) {
+    throw new Error('For i18n usage you must install the package, using npm i vue-i18n@latest')
+  }
+
   const doctype = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
   const app = createApp({ render: () => h(component) }, props)
+
+  if (options?.i18n?.defaultLocale && vueI18n) {
+    const i18n = vueI18n.createI18n({
+      locale: options?.i18n?.locale,
+      fallbackLocale: options?.i18n.defaultLocale,
+      messages: options?.i18n.translations,
+    })
+
+    app.use(i18n)
+  }
+
   const markup = await renderToString(app)
 
   if (options.plainText) {
