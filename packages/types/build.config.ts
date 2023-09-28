176ca3f40951262013180f657b6c2d8b5a6938bd
David Arenas <davejs136@gmail.com>
2023-09-28 14:37:42 -0400

    feat: register packages, add types pkg

---
+++ packages/types/build.config.ts
@@ -0,0 +1,27 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  declaration: true,
+  clean: true,
+  rollup: {
+    emitCJS: true,
+    inlineDependencies: true,
+  },
+  entries: [
+    {
+      input: 'src/index.ts',
+      outDir: 'dist',
+      name: 'index',
+      format: 'esm',
+      ext: 'mjs',
+    },
+    {
+      input: 'src/index.ts',
+      outDir: 'dist',
+      name: 'index',
+      format: 'cjs',
+      ext: 'js',
+    },
+  ],
+  externals: ['vue'],
+})
