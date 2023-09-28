ed7f5e838c8a77700768fd8fbeec074c41fcdec8
David Arenas <davejs136@gmail.com>
2023-09-28 15:55:21 -0400

    fix: solve build issues

--- packages/utils/build.config.ts
+++ packages/utils/build.config.ts
@@ -23,5 +23,5 @@ export default defineBuildConfig({
       ext: 'js',
     }
   ],
-  externals: ['esbuild']
+  externals: ['esbuild', 'vue']
 })
--- packages/utils/src/markdown/index.ts
+++ packages/utils/src/markdown/index.ts
@@ -1,3 +1,3 @@
-export * from './markdown'
+export * from './utils'
 export * from './patterns'
 export * from './styles'
