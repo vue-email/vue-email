b2d513713fd2ffbd517e0e0e36c9f34ce397e5f4
David Arenas <davejs136@gmail.com>
2023-09-29 11:57:39 -0400

    feat: add compiler tests

---
+++ packages/compiler/test/templates/ScriptSetup.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+  name: String
+})
+</script>
+
+<template>
+  <section>
+    <h1>Welcome {{ name }}</h1>
+  </section>
+</template>
---
+++ packages/compiler/test/templates/UseOtherComponents.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>
+    <Hello>
+      Content inside Hello
+    </Hello>
+  </section>
+</template>
