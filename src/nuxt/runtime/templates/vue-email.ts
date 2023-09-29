b41eef70e39a0a689498fa8458ec95ccd21b6456
David Arenas <davejs136@gmail.com>
2023-09-29 12:28:59 -0400

    chore: remove older `src` directory

--- src/nuxt/runtime/templates/vue-email.ts
+++
@@ -1,9 +0,0 @@
-import { VueEmailPlugin, VueEmailPluginOptions } from 'vue-email'
-import {defineNuxtPlugin, useRuntimeConfig } from "#app"
-
-export default defineNuxtPlugin((nuxtApp) => {
-  const config = useRuntimeConfig()
-  const options = config.public['vueEmailOptions'] as VueEmailPluginOptions
-
-  nuxtApp.vueApp.use(VueEmailPlugin, options)
-})
