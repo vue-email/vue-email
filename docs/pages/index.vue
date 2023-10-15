a0e83fb8f405d8f336ca0404767a2a4bd0b1e507
David Arenas <davejs136@gmail.com>
2023-10-15 17:28:01 -0400

    fix(docs): LandingCard component error

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -79,37 +79,37 @@ const { format } = Intl.NumberFormat('en-GB', { notation: 'compact' })
   <ULandingSection>
     <template #title> Build and send emails using <br /><span class="text-primary-400">Vue and TypeScript.</span> </template>
     <UPageGrid>
-      <ULandingCard
+      <CustomLandingCard
         to="/getting-started/ssr"
         title="SSR Support"
         icon="i-solar-server-2-bold-duotone"
         description="Streamline the process of rendering and dispatching emails directly from your server."
       />
-      <ULandingCard
+      <CustomLandingCard
         to="/getting-started/i18n"
         title="i18n Support"
         icon="i-solar-globus-bold-duotone"
         description="Sprinkle some global flavor onto your vue email templates with our new i18n feature!"
       />
-      <ULandingCard
+      <CustomLandingCard
         to="/components/html"
         title="15+ Components"
         icon="i-ph-plug-duotone"
         description="Simplify email creation by providing ready-made components, eliminating the need for table-based layouts and outdated markup."
       />
-      <ULandingCard
+      <CustomLandingCard
         to="/integrations/mailersend"
         title="Integrations"
         icon="i-solar-bolt-line-duotone"
         description="Use your favorite email provider with VueEmail. Any Node.js email provider can be used with VueEmail."
       />
-      <ULandingCard
+      <CustomLandingCard
         to="/getting-started/devtools"
         title="Nuxt Devtools"
         icon="i-ph-magic-wand-duotone"
         description="Inspect your email templates and send test emails from your app directly."
       />
-      <ULandingCard
+      <CustomLandingCard
         to="/components/tailwind"
         title="Tailwind CSS"
         icon="i-solar-palette-round-line-duotone"
