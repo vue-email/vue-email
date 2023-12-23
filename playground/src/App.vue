7ebf762700be7e39c6355724dc09d6fb79e38d8a
Younes Barrad <younesbarraddev@gmail.com>
2023-12-23 14:21:18 +0100

    chore: updated playground

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -2,10 +2,11 @@
 import { onMounted, ref } from 'vue'
 import { useRender } from 'vue-email'
 import MyEmail from './components/MyEmail.vue'
+import Test from './components/Test.vue'
 
 const email = ref('')
 onMounted(async () => {
-  await useRender(MyEmail, { props: { title: 'Some title' } }, {
+  await useRender(Test, { props: { title: 'Some title' } }, {
     pretty: true,
   }).then((res) => {
     email.value = res.html
