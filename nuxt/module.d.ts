9d931f9a50e36abeda10ebd769a7cd460f19a341
Younes Barrad <younesbarraddev@gmail.com>
2024-06-04 14:11:20 +0100

    chore: removed nuxt module

--- nuxt/module.d.ts
+++
@@ -1,8 +0,0 @@
-import type * as _nuxt_schema from '@nuxt/schema'
-
-declare const _default: _nuxt_schema.NuxtModule<{
-  baseUrl?: string | null
-  autoImport?: boolean
-}>
-
-export { _default as default }
